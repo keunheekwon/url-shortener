9c2894f01f28f4d9a9af5dc60a96f62dbea5df40
keunheekwon <keunheekwon6341@gmail.com>
2025-04-03 21:41:32 +0900

    style: 주석 삭제

--- src/app.ts
+++ src/app.ts
@@ -9,21 +9,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// JSON 본문 파싱을 위한 미들웨어 추가
-app.use(express.json()); // POST 요청의 body를 JSON 형식으로 파싱
+app.use(express.json());
 
-// 정적 파일을 서빙할 디렉토리 설정 (public 폴더)
 app.use(express.static(path.join(__dirname, "", "public")));
 
-// 루트 경로에서 index.html 파일을 서빙
 app.get("/", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
 
-// URL 단축 라우터 설정
 app.use(urlRoutes);
 
-// Mongoose 연결
 mongoose
   .connect(process.env.MONGO_URI || "mongodb://localhost:27017/url_shortener", {
     useNewUrlParser: true,
